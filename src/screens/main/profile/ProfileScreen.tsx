ab7a36e62bb2b868e7830c20b2e419ae0d39fe10
Dave Santiago <dave.santiago25@gmail.com>
2025-12-17 23:49:15 +0800

    basic list

--- src/screens/main/profile/ProfileScreen.tsx
+++ src/screens/main/profile/ProfileScreen.tsx
@@ -1,8 +1,21 @@
 import React from "react";
-import { View, Text, StyleSheet, Button } from "react-native";
+import { View, Text, StyleSheet, FlatList } from "react-native";
 import { useNavigation } from '@react-navigation/native'; 
 import type { NativeStackNavigationProp } from '@react-navigation/native-stack';
 import { RootStackParamList } from '../../../navigation/RootNavigator';
+
+const menuArray = [
+  { id: "1", title: "My Information" },
+  { id: "2", title: "Social Accounts" },
+  { id: "3", title: "Linked Cards" },
+  { id: "4", title: "Promo Codes" },
+  { id: "5", title: "Order History" },
+  { id: "6", title: "Invite Friends" },
+  { id: "7", title: "Referrals" },
+  { id: "8", title: "FAQs" },
+  { id: "9", title: "Terms and Privacy Policy" },
+  { id: "10", title: "Log Out" },
+];
 
 export default function ProfileScreen() {
 
@@ -10,22 +23,31 @@ export default function ProfileScreen() {
 
   return (
     <View style={styles.container}>
-      <Text>Profile</Text>
-
-      <Button
-        title="My Information"
-        onPress={() => navigation.navigate('MyInformation', { id: '232323', name: 'Clark' }) }
+      <FlatList
+      data={menuArray}
+      keyExtractor={(item) => item.id}
+      renderItem={({ item }) => (
+        <View style={styles.item}>
+          <Text style={styles.text}>{item.title}</Text>
+        </View>
+      )}
       />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#eff2f4ff',
-    paddingHorizontal: 20,
+  container: { 
+    flex: 1, 
+    padding: 20 
+  },
+  item: {
+    padding: 16,
+    marginBottom: 10,
+    backgroundColor: "#f1f1f1",
+    borderRadius: 8,
+  },
+  text: { 
+    fontSize: 16 
   }
 });
