8478a2cd8accb7325ec260fced50d06a783d3eba
Dave Santiago <dave.santiago25@gmail.com>
2026-01-03 03:05:49 +0800

    banner example (in progress)

--- src/screens/main/dashboard/DashboardScreen.tsx
+++ src/screens/main/dashboard/DashboardScreen.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { View, Text, StyleSheet, Button } from 'react-native';
+import { View, StyleSheet, FlatList, Image, Dimensions } from 'react-native';
 import { BottomTabNavigationProp } from "@react-navigation/bottom-tabs";
 import { HomeTabsNavigatorParamList } from "../../../navigation/HomeTabsNavigator";
 import AppButton from "../../../components/AppButton";
@@ -8,11 +8,32 @@ type Props = {
   navigation: BottomTabNavigationProp<HomeTabsNavigatorParamList>;
 };
 
+const { width } = Dimensions.get('window');
+
+const banners = [
+  { id: '1', image: 'https://picsum.photos/200/300' },
+  { id: '2', image: 'https://picsum.photos/200/300' },
+  { id: '3', image: 'https://picsum.photos/200/300' },
+];
+
 export default function DashboardScreen({ navigation }: Props) {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Dashboard</Text>
-      <AppButton titleText="Go to Profile" onPressed={() => navigation.navigate("Profile")} />
+      <View style={styles.bannerContainer}>
+        <FlatList
+        data={banners}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => (
+          <Image source={{ uri: item.image }} style={styles.banner} />
+        )}
+        />
+      </View>
+      <View style={styles.content}>
+        <AppButton titleText="Go to Profile" onPressed={() => navigation.navigate("Profile")} />
+      </View>
+      
     </View>
   );
 }
@@ -20,15 +41,21 @@ export default function DashboardScreen({ navigation }: Props) {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
     backgroundColor: '#ffffff',
-    paddingHorizontal: 20,
   },
-  text: {
-    fontSize: 22,
-    fontWeight: '600',
-    color: '#333',
-    marginBottom: 30,
+  content: {
+    flex: 1,
+    paddingHorizontal: 16,
+    alignItems: 'center'
+  },
+  bannerContainer: {
+    marginLeft: 16
+  },
+  banner: {
+    marginTop: 16,
+    width,
+    height: 200,
+    borderRadius: 12,
+    marginRight: 16
   }
 });
