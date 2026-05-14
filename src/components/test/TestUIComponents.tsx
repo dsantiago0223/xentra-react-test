d69febd1e64932861887f0ca670be42d8c5d277c
Dave Santiago <dave.santiago25@gmail.com>
2026-05-14 09:01:12 +0800

    est bitrise build android only

--- src/components/test/TestUIComponents.tsx
+++ src/components/test/TestUIComponents.tsx
@@ -61,7 +61,7 @@ export default function TestUIComponents({ navigation }: Props) {
         />
 
         <LayoutNavigationHeader
-          titleText="Header Title"
+          titleText="Nav Header Title"
           titleLogo
           onLeftPressed={() => navigation.goBack()}
           onRightPressed={() => navigation.goBack()}
